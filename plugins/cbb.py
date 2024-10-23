6e91c2a39ef71abeb8fa02bccd36ac3a3ad040ca
Tony Stark <138078424+TonyStarkBotz@users.noreply.github.com>
2024-10-24 00:58:20 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -11,7 +11,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     if data == "about":
         await query.message.edit_text(
             text = f"<b>⋉ Creator : Ƭᴏɴʏ Ꮪᴛᴀʀᴋ ⎊\n❐ Support Group: @MovieTimesXDisc\n❐ Powered by: @TonyStark_Botz\n
-━━━━━━━━━━━━━━━━━━━━━━━━━━━\n♤ ᴘʀᴇsᴇɴᴛᴇᴅ ʙʏ <a href='https://t.me/MovieTimes_TV'>Mᴏᴠɪᴇ Tɪᴍᴇs™</a></b>",
+━━━━━━━━━━━━━━━━━━━━━━━━━━━\n♤ ᴘʀᴇsᴇɴᴛᴇᴅ ʙʏ <a href='https://t.me/MovieTimes_TV'>Mᴏᴠɪᴇ Tɪᴍᴇs™</a></b>"
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
